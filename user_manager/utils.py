b108e452b808ecb972a7b7b7ae45a474bca026cc
german <german@nauticadigital.co>
2019-05-22 12:21:10 -0500

    Ajuste para que funcione la clase selected de apps.

--- user_manager/utils.py
+++ user_manager/utils.py
@@ -14,6 +14,7 @@ def get_apps_list():
             'app_name': app['app_name'],
             'app_id': app['app_id'],
             'roles': app['roles'].split(','),
+            'selected': False,
             'selected_role': ''
         }
         app_list.append(app_dict)
