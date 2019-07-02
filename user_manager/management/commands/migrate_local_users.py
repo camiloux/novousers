b22a183d48c169ef31018a9cb7c8d03065a6f433
german <german@nauticadigital.co>
2019-07-02 17:35:03 -0500

    Management command para enviar correo de reset a todos los usuarios.

---
+++ user_manager/management/commands/migrate_local_users.py
@@ -0,0 +1,26 @@
+from django.core.management import BaseCommand
+
+from user_manager.auth0utils import get_all_users, request_password_reset
+
+
+class Command(BaseCommand):
+    help = 'Send reset email to all users'
+
+    def handle(self, *args, **options):
+        users = get_all_users()
+        users_with_errors = []
+        emails_sent = 0
+
+        for user in users:
+            self.stdout.write(user)
+            # result, status = request_password_reset(user['username'], user['email'])
+            # if status == 200:
+            #     emails_sent += 1
+            # else:
+            #     users_with_errors.append('{} - {}'.format(user, result))
+
+        if len(users_with_errors) > 0:
+            self.stdout.write('Could not send reset email to {} users'.format(len(users_with_errors)))
+            for u in users_with_errors:
+                self.stdout.write(u)
+        self.stdout.write('{} email{} sent.'.format(emails_sent, '' if emails_sent == 1 else ''))
